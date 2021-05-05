a05cb00c91b57f992fb3becaa7e189be0d4acd37
Wayne W Cook <Wayne.Cook@coloradoearlycolleges.org>
2021-05-05 10:04:35 -0600

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/* HelloWorld.java
+ * Wayne Cook
+ * 5 January 2021
+ * Purpose:
+ *      Print out the Words "Hello World" to the console.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
